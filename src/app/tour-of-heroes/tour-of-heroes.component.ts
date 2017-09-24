73285852ec949cbd012acb49f62950c8f9689d2b
malv <malvin@danhof.de>
2017-09-25 00:44:17 +0200

    added routerLinkActive to tour-of-heroes

--- src/app/tour-of-heroes/tour-of-heroes.component.ts
+++ src/app/tour-of-heroes/tour-of-heroes.component.ts
@@ -3,18 +3,18 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'tour-of-heroes',
     template: `
-        <h1 class="title smooth">{{title}}</h1>
-        <a routerLink="/dashboard" class="button">Dashboard</a>
-        <a routerLink="/heroes" class="button">Heroes</a>
+        <h1 class="title">{{title}}</h1>
+        <a routerLink="/dashboard" class="button" routerLinkActive="is-primary black">Dashboard</a>
+        <a routerLink="/heroes" class="button" routerLinkActive="is-primary black">Heroes</a>
         <div [ngStyle]="{ 'margin-top': '30px'}"></div>
         <router-outlet></router-outlet>
     `,
     styles: [`
-        .smooth {
-            text-shadow: 1px 10px 20px #ffdd57, 0 0 25px #ffdd57, 0 0 5px #ffdd57;
+        .black {
+            color: black;
         }
     `]
 })
 export class TourOfHeroesComponent { 
-    title = 'heroes!';
+    title = 'ng heroes';
 }
